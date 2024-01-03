dd9e0e1b8afd2d89d734b408741fc76288cb2e97
Elik <eliseyignatchenko06@gmail.com>
2024-01-03 19:50:18 +0200

    calculatorUpgrade

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const age = 20;
-const hd = false;
-let ticket
-if (age < 0 || typeof age !== 'number'){
-    console.log('nekor')
-} else if(age <= 6){
-ticket = 0 ;
-}else if( age <= 12 ){
-    ticket = 10;
-}else if (!hd){
-    ticket = 20;
-}else {
-    ticket = 10;
+function calculator(num1, num2, functionLink) {
+  let results = functionLink(num1, num2);
+  return results;
 }
-if (ticket !== undefined){
-console.log('vartist : ' + ticket + '$' );
+console.log(calculator(5, 5, plus));
+
+console.log(calculator(3, 2, function (a, b) {
+  return a**b;
+}));
+function minus(a, b) {
+  return a - b;
+}
+function plus(a, b) {
+  return a + b;
 }
