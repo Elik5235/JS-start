da32fcbc00a71c7990b5b07f484ad2df5a2f6b4e
Elik <eliseyignatchenko06@gmail.com>
2024-01-05 23:16:08 +0200

    deadmachine

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-function calculator(num1, num2, functionLink) {
-  let results = functionLink(num1, num2);
-  return results;
-}
-console.log(calculator(5, 5, plus));
-
-console.log(calculator(3, 2, function (a, b) {
-  return a**b;
-}));
-function minus(a, b) {
-  return a - b;
-}
-function plus(a, b) {
-  return a + b;
+let i = 2;
+while(true){
+  i += 2;
+  console.log(i)
 }
