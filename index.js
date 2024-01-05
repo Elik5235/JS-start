dce95e9312e84e0b012767af69ada0335d8e8fec
Elik <eliseyignatchenko06@gmail.com>
2024-01-06 01:06:36 +0200

    fuctorial

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const cp = 'one123';
-let pin = prompt ('pin its');
-while (pin !== cp){
-  pin = prompt("she ras")
+function factorial (number){
+  let res = 1
+  if (number === 1 || number === 0){
+return 1;
+  }
+  else if (number < 0){
+    return null;
+  }
+  for (let it = 1;it <= number; it++){
+    res = res * it;
+  }
+  return res;
 }
- 
+console.log (factorial(170));
 
   
