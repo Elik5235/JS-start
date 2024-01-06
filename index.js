9e48044f5d83abeb73931bf8392c918be1ee2f6b
Elik <eliseyignatchenko06@gmail.com>
2024-01-06 17:45:20 +0200

    FizzBuzzGame

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function factorial (number){
-  let res = 1
-  if (number === 1 || number === 0){
-return 1;
+function fizzBuzzGame(limit) {
+  for (let i = 1; i <= limit; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (1 % 3 === 0) {
+      console.log("Fizz");
+    } else if (1 % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
   }
-  else if (number < 0){
-    return null;
-  }
-  for (let it = 1;it <= number; it++){
-    res = res * it;
-  }
-  return res;
 }
-console.log (factorial(170));
-
-  
+fizzBuzzGame(100);
