9eb1fac218fef67a72414333d7e317b6985feb61
Elik <eliseyignatchenko06@gmail.com>
2024-01-09 18:20:09 +0200

    q2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-function Federation(title, popul, area) {
-  this.title = title;
-  this.popul = popul;
-  this.area = area;
+function greetingUser(user){
+ return ` hey ${user.name} ${user.surname} ${user.age + 1 }`}
 
-  this.getTitle = function () {
-    return this.popul / this.area;
-  };
+const user = {
+  name: 'john',
+  surname: 'Doe',
+  age: 19
 }
-const federation1 = new Federation("США",20, 50);
+greetingUser(user) 
