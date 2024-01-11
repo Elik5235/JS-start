11c29f9e21da6f0a531ab44d20caa1b456d518fc
Elik <eliseyignatchenko06@gmail.com>
2024-01-11 15:14:46 +0200

    2df

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function greetingUser(user){
- return ` hey ${user.name} ${user.surname} ${user.age + 1 }`}
-
-const user = {
-  name: 'john',
-  surname: 'Doe',
-  age: 19
+const departmentSalary = {
+  HR: 120000,
+  development: 100000,
+  PR: 1200,
+  MARK: 21211,
+  asisstant: undefined,
+};
+function greet(departmentSalary) {
+  let sum = 0;
+  let nameOfUndefined;
+  for (let key in departmentSalary) {
+    if (typeof departmentSalary[key] !== undefined) {
+      sum += departmentSalary[key];
+    } else {
+      nameOfUndefined = key;
+      console.log("blablabla"`${nameOfUndefined}`);
+    }
+  }
+  return sum;
 }
-greetingUser(user) 
+console.log(greet(departmentSalary));
