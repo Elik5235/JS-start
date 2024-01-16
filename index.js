0c994c56c5d915b2398295bfbd69fe1a9de1e363
Elik <eliseyignatchenko06@gmail.com>
2024-01-16 20:14:41 +0200

    prr

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const oldarr = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
-const newarray = oldarr.map((item) => {
-   return item * 2;
-  })
-.reverse().join(',');
+const numaArr = [5,3,-9,21,5,1,-2,4]
+// function filter(item){
+// if (item % 2 === 0){
+// return true;
+// }
+// return false;
+// }
+// console.log(filter(1))
+function filter(item){
+return item % 2 === 0
+}
+console.log(filter.numaArr)
